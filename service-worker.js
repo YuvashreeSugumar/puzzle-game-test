5b567abf24b3c8c7c172ddc88b38158289d4214c
YUVASHREE S <141635194+YuvashreeSugumar@users.noreply.github.com>
2025-08-16 21:16:47 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('kpop-rush').then(c=>c.addAll(['.','index.html','style.css','game.js'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
